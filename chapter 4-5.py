75f840b242c74f805de2920d20b9341fce394a2d
LesleyDong <35983724+LesleyDong@users.noreply.github.com>
2018-03-29 15:52:47 +0800

    Update chapter 4-5.py

--- chapter 4-5.py
+++ chapter 4-5.py
@@ -32,3 +32,48 @@ range(2, 41, 3)
 35
 38
 >>> # Squence[start: end : step]    range(start, end, step)
+
+
+
+#4.4.5-----------------------------------------------------------------------------------
+>>> Ellipsis
+Ellipsis
+>>> None
+>>>
+>>> list[1:Ellipsis:30]
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: slice indices must be integers or None or have an __index__ method
+>>> list[1:None:30]
+range(2, 80, 30)
+>>>
+>>> ...
+Ellipsis
+
+
+#4.5.2-----------------------------------------------------------------------------------
+>>> a = 1
+>>> b = a
+>>> a is b
+True
+>>>
+>>> c = 1
+>>> a is c
+True
+>>> d = 0.5 + 0.5
+>>> a is d
+False
+>>>
+
+
+#4.6.4-----------------------------------------------------------------------------------
+>>> def func(a):
+...     if isinstance(a, (int)):
+...             print("TURE")
+...
+>>> a = 3
+>>> func(a)
+TURE
+>>> a=[1,2,3]
+>>> func(a)
+>>>
