87aea2e996017a57133ba49ee62c52bea1f36c62
LesleyDong <35983724+LesleyDong@users.noreply.github.com>
2018-03-29 15:33:57 +0800

    Update chapter 4-5.py

--- chapter 4-5.py
+++ chapter 4-5.py
@@ -4,3 +4,31 @@
 >>> type(type)
 <class 'type'>
 >>>
+
+
+#4.3.4-----------------------------------------------------------------------------------
+>>> list = range(1:80)
+  File "<stdin>", line 1
+    list = range(1:80)
+                  ^
+SyntaxError: invalid syntax
+>>> list = range(1,80)
+>>> list[1:40:3]
+range(2, 41, 3)
+>>> for i in list[1:40:3]:
+...     print(i)
+...
+2
+5
+8
+11
+14
+17
+20
+23
+26
+29
+32
+35
+38
+>>> # Squence[start: end : step]    range(start, end, step)
